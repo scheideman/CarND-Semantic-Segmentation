2cff954d462cbeff811abcbd1c109aee20108a37
sean <searn@ualberta.ca>
2017-08-20 08:28:31 -0600

    started on project

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os.path
 import tensorflow as tf
+tf.GraphKeys.VARIABLES = tf.GraphKeys.GLOBAL_VARIABLES
 import helper
 import warnings
 from distutils.version import LooseVersion
@@ -32,8 +33,15 @@ def load_vgg(sess, vgg_path):
     vgg_layer3_out_tensor_name = 'layer3_out:0'
     vgg_layer4_out_tensor_name = 'layer4_out:0'
     vgg_layer7_out_tensor_name = 'layer7_out:0'
+
+    tf.saved_model.loader.load(sess,[vgg_tag],vgg_path)
+    image_input = sess.graph.get_tensor_by_name(vgg_input_tensor_name)
+    keep_prob = sess.graph.get_tensor_by_name(vgg_keep_prob_tensor_name)
+    layer3_out = sess.graph.get_tensor_by_name(vgg_layer3_out_tensor_name)
+    layer4_out = sess.graph.get_tensor_by_name(vgg_layer4_out_tensor_name)
+    layer7_out = sess.graph.get_tensor_by_name(vgg_layer7_out_tensor_name)
     
-    return None, None, None, None, None
+    return image_input, keep_prob, layer3_out, layer4_out, layer7_out
 tests.test_load_vgg(load_vgg, tf)
 
 
@@ -47,7 +55,20 @@ def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
     :return: The Tensor for the last layer of output
     """
     # TODO: Implement function
-    return None
+    
+
+    # need to check shape
+    output_1x1 = tf.layers.conv2d(vgg_layer7_out, num_classes, 1, strides=(1,1))
+    output_decode1 = tf.layers.conv2d_transpose(output_1x1, num_classes, 4, strides=(2, 2)) 
+    vgg_layer4_out=tf.reshape(vgg_layer4_out, tf.shape(output_decode1), name=None)
+    output_skip1 = tf.add(output_decode1, vgg_layer4_out)
+    output_decode2 = tf.layers.conv2d_transpose(output_skip1, num_classes, 4, strides=(2, 2))
+    vgg_layer3_out=tf.reshape(vgg_layer3_out, tf.shape(output_decode2), name=None) 
+    output_skip2 = tf.add(output_decode2, vgg_layer3_out)
+    output_decode3 = tf.layers.conv2d_transpose(output_skip2, num_classes, 16, strides=(8, 8))
+    
+
+    return output_decode3
 tests.test_layers(layers)
 
 
@@ -61,9 +82,17 @@ def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
     :return: Tuple of (logits, train_op, cross_entropy_loss)
     """
     # TODO: Implement function
-    return None, None, None
+    logits = tf.reshape(nn_last_layer, (-1, num_classes))
+    cross_entropy_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits,  labels=correct_label))
+    optimizer = tf.train.AdamOptimizer(learning_rate = learning_rate)
+    training_operation = optimizer.minimize(cross_entropy_loss)
+    return logits, training_operation, cross_entropy_loss
 tests.test_optimize(optimize)
 
+def mean_iou(ground_truth, prediction, num_classes):
+    # TODO: Use `tf.metrics.mean_iou` to compute the mean IoU.
+    iou, iou_op = tf.metrics.mean_iou(ground_truth, prediction, num_classes)
+    return iou, iou_op
 
 def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_loss, input_image,
              correct_label, keep_prob, learning_rate):
@@ -80,9 +109,18 @@ def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_l
     :param keep_prob: TF Placeholder for dropout keep probability
     :param learning_rate: TF Placeholder for learning rate
     """
+    sess.run(tf.global_variables_initializer())
+    # need to initialize local variables for this to run `tf.metrics.mean_iou`
+    sess.run(tf.local_variables_initializer())
     # TODO: Implement function
-    pass
+    for i in range(epochs):
+        batches = get_batches_fn(batch_size)
+        for batch_x, batch_y in batches:
+            sess.run(train_op, feed_dict={input_image: batch_x, correct_label: batch_y, keep_prob: 0.5, learning_rate: 0.001})
+    
 tests.test_train_nn(train_nn)
+
+
 
 
 def run():
@@ -91,7 +129,8 @@ def run():
     data_dir = './data'
     runs_dir = './runs'
     tests.test_for_kitti_dataset(data_dir)
-
+    epochs = 1
+    batch_size = 2
     # Download pretrained vgg model
     helper.maybe_download_pretrained_vgg(data_dir)
 
@@ -99,7 +138,11 @@ def run():
     # You'll need a GPU with at least 10 teraFLOPS to train on.
     #  https://www.cityscapes-dataset.com/
 
+    y = tf.placeholder(tf.int32, (None))
+    lr = tf.placeholder(tf.float32)
+
     with tf.Session() as sess:
+
         # Path to vgg model
         vgg_path = os.path.join(data_dir, 'vgg')
         # Create function to get batches
@@ -109,9 +152,12 @@ def run():
         #  https://datascience.stackexchange.com/questions/5224/how-to-prepare-augment-images-for-neural-network
 
         # TODO: Build NN using load_vgg, layers, and optimize function
-
+        image_input, keep_prob, layer3_out, layer4_out, layer7_out = load_vgg(sess,vgg_path)
+        decoder = layers(layer3_out, layer4_out, layer7_out,num_classes)
+        logits, training_operation, cross_entropy_loss = optimize(decoder, y, lr, num_classes)
         # TODO: Train NN using the train_nn function
-
+        train_nn(sess, epochs, batch_size, get_batches_fn, training_operation, cross_entropy_loss, image_input,
+             y, keep_prob, lr)
         # TODO: Save inference data using helper.save_inference_samples
         #  helper.save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_prob, input_image)
 
