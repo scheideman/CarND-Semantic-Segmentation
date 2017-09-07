3b75131cf7e05ce01c983d31ae8a9211fed68e8c
sean <searn@ualberta.ca>
2017-09-06 22:43:35 -0600

    cleanup

--- main.py
+++ main.py
@@ -26,8 +26,6 @@ def load_vgg(sess, vgg_path):
     :param vgg_path: Path to vgg folder, containing "variables/" and "saved_model.pb"
     :return: Tuple of Tensors from VGG model (image_input, keep_prob, layer3_out, layer4_out, layer7_out)
     """
-    # TODO: Implement function
-    #   Use tf.saved_model.loader.load to load the model and weights
     vgg_tag = 'vgg16'
     vgg_input_tensor_name = 'image_input:0'
     vgg_keep_prob_tensor_name = 'keep_prob:0'
@@ -57,11 +55,6 @@ def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
     :param num_classes: Number of classes to classify
     :return: The Tensor for the last layer of output
     """
-    # TODO: Implement function
-    
-
-    # need to check shape
-    #strides=(1,1) 
     output_1x1_1 = tf.layers.conv2d(vgg_layer7_out, num_classes, 1, padding='same', 
                                         kernel_regularizer=tf.contrib.layers.l2_regularizer(0.001),
                                         kernel_initializer=tf.truncated_normal_initializer(stddev=0.01))
@@ -97,7 +90,6 @@ def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
     :param num_classes: Number of classes to classify
     :return: Tuple of (logits, train_op, cross_entropy_loss)
     """
-    # TODO: Implement function
     logits = tf.reshape(nn_last_layer, (-1, num_classes))
     cross_entropy_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits,  labels=correct_label))
     optimizer = tf.train.AdamOptimizer(learning_rate = learning_rate)
@@ -106,7 +98,6 @@ def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
 tests.test_optimize(optimize)
 
 def mean_iou(ground_truth, prediction, num_classes):
-    # TODO: Use `tf.metrics.mean_iou` to compute the mean IoU.
     iou, iou_op = tf.metrics.mean_iou(ground_truth, prediction, num_classes)
     return iou, iou_op
 
@@ -180,9 +171,6 @@ def run():
              y, keep_prob, lr)
         # TODO: Save inference data using helper.save_inference_samples
         helper.save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_prob, image_input)
-
-        # OPTIONAL: Apply the trained model to a video
-        
 
 
 if __name__ == '__main__':
